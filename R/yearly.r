373ec10ddc0699ced1fd9cedd92d4a33c5615406
Jared Lander <info@jaredlander.com>
2011-11-27 15:50:52 -0500

    Code to grab the URL of the boxscores for each year going from 1940 through 2010

--- R/yearly.r
+++ R/yearly.r
@@ -1,8 +1,20 @@
 ## This code figures out how many games were in each season and then the links to those weeks
 require(RCurl)
 require(XML)
-tester <- RCurl::getURL(url="http://www.pro-football-reference.com/years/2002/games.htm")
+require(plyr)
+tester <- RCurl::getURL(url="http://www.pro-football-reference.com/years/2010/games.htm")
 
-years <- 
-boxes <- str_extract_all(string=tester, pattern="boxscores/[0-9]{9}[a-z]{3}\\.htm")[[1]]
-length(boxes)
+years <- 1940:2010
+
+getBoxURL <- function(theYear)
+{
+    theURL <- sprintf("http://www.pro-football-reference.com/years/%s/games.htm", theYear)
+    thePage <- RCurl::getURL(url=theURL)
+    
+    boxes <- str_extract_all(string=thePage, pattern="boxscores/[0-9]{9}[a-z]{3}\\.htm")[[1]]
+    
+    return(data.frame(URL=boxes))
+}
+
+system.time(allYears <- adply(years, 1, getBoxURL))
+allYears
